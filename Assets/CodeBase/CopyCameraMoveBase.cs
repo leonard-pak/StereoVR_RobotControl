36c7e6c218eafd24e332a2110eaca2f62ff74e02
Leonard <leopak2000@gmail.com>
2021-11-09 17:23:11 +0300

    Prepared to add a ROS

---
+++ Assets/CodeBase/CopyCameraMoveBase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace CodeBase
+{
+    public abstract class CopyCameraMoveBase : MonoBehaviour
+    {
+        protected Transform TrackedObject;
+        protected virtual void Awake()
+        {
+            if (Camera.main is { })
+            {
+                TrackedObject = Camera.main.transform;
+            }
+        }
+
+        protected virtual void Update()
+        {
+            SpecialMoveOnUpdate();
+        }
+
+        protected abstract void SpecialMoveOnUpdate();
+    }
+}
---
+++ Assets/CodeBase/ObjectsMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace CodeBase
+{
+    public class ObjectsMove : CopyCameraMoveBase
+    {
+        public bool TrackRotation;
+        public bool TrackPosition;
+        [SerializeField] private float RotateSpeed;
+
+        protected override void SpecialMoveOnUpdate()
+        {
+            if (TrackRotation)
+            {
+                if (RotateSpeed > 0)
+                {
+                    transform.localRotation = Quaternion.Lerp(transform.rotation, TrackedObject.rotation, Mathf.SmoothStep(0.0f, 1.0f, RotateSpeed));
+                }
+                else
+                {
+                    if (transform.parent != TrackedObject)
+                    {
+                        transform.localRotation = TrackedObject.rotation;
+                    }
+                }
+            }
+
+            if (TrackPosition)
+            {
+                transform.position = TrackedObject.position;
+            }
+        }
+    }
+}
